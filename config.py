7ca105a1b00ec3a1e407b2dcfcaa4babd7862964
Professor Knight <profdevent@gmail.com>
2026-01-20 20:05:03 -0500

    fix linting err'

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from typing import Optional
 
 class Settings(BaseSettings):
     db_user: str
-      db_password: str
-      db_name: str
-      db_host: str = "db"
-      db_port: int = 5432
+    db_password: str
+    db_name: str
+    db_host: str = "db"
+    db_port: int = 5432
 
       @computed_field
       @property
